75bea30d755894dd15ddb5853e5fca772c5c369d
mahmut erkul <mahmuterkul1907@gmail.com>
2016-05-13 21:22:54 +0300

    Initial Commit

---
+++ AoA_P3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AoA_P3
+//
+//  Created by Mahmut Erkul on 13/05/16.
+//  Copyright © 2016 Mahmut Erkul. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
